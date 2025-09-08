83840aafe553b13b384a2c7696546d299c1600d2
jack10 <76940581+Harivaradhan@users.noreply.github.com>
2025-09-09 01:46:26 +0530

    Update JobList.js

--- src/Components/JobList.js
+++ src/Components/JobList.js
@@ -5,7 +5,7 @@ function JobList() {
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8080/management/api/jobs")
+    fetch("https://jobmanagement-server.onrender.com/api/jobs")
       .then((res) => res.json())
       .then((data) => setJobs(data))
       .catch((err) => console.error("Error fetching jobs:", err));
